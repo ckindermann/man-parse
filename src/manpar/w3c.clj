4684f6787edf6376966327956f472e65cb0082e2
chris <chris.kind.man@gmail.com>
2021-08-23 00:05:36 +0100

    Add handling of whitspace

--- src/manpar/w3c.clj
+++ src/manpar/w3c.clj
@@ -7,26 +7,33 @@
 (def parser
   (insta/parser
    "S = Description 
-    Description = Conjunction <' or '> Conjunction { <' or '> Conjunction }
+    Description = Conjunction <WS> <'or'> <WS> Conjunction {<WS> <'or'> <WS> Conjunction }
                 | Conjunction
-    Conjunction = classIRI ' that ' [ ' not ' ] Restriction { ' and ' [ 'not ' ] Restriction }
-                | Primary <' and '>  Primary { <' and '> Primary }
+    Conjunction = classIRI <WS> 'that' <WS> [ 'not' <WS> ] Restriction { <WS> 'and' <WS> [ 'not' <WS> ] Restriction }
+                | Primary <WS> <'and'> <WS>  Primary { <WS> <'and'> <WS> Primary }
                 | Primary
-    <Primary> = [ 'not ' ] ( Restriction | atomic )
+    <Primary> = [ 'not' <WS> ] ( Restriction | atomic )
 
-    Restriction = ObjectPropertyExpression <' some '> Primary 
-                | ObjectPropertyExpression <' only '> Primary
-                | ObjectPropertyExpression <' min '> NonNegativeInteger <' '> [ Primary ]
-                | ObjectPropertyExpression <' max '> NonNegativeInteger <' '> [ Primary ]
-                | ObjectPropertyExpression <' exactly '> NonNegativeInteger <' '> [ Primary ]
+    Restriction = ObjectPropertyExpression <WS> <'some'> <WS> Primary 
+                | ObjectPropertyExpression <WS> <'only'> <WS> Primary
+                | ObjectPropertyExpression <WS> <'min'> <WS> NonNegativeInteger <WS> [ Primary ]
+                | ObjectPropertyExpression <WS> <'max'> <WS> NonNegativeInteger <WS> [ Primary ]
+                | ObjectPropertyExpression <WS> <'exactly'> <WS> NonNegativeInteger <WS> [ Primary ]
+                | ObjectPropertyExpression <WS> 'Self'
+                | ObjectPropertyExpression <WS> 'value' <WS> Literal
+
     <atomic> = classIRI
            | <'('> Description <')'>
+
+    WS = #'\\s+' 
+
     NonNegativeInteger = Zero | PositiveInteger
     PositiveInteger = NonZero { digit }
     digit = Zero | NonZero
     Zero = '0'
     NonZero = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' 
 
+    Literal = #'.*'
     classIRI = IRI
     objectPropertyIRI = IRI
     <IRI> = #'[^ ()\\s]*'
